fc9fb13725b55920123aaeddb89f994d2480fb0c
Shiva112200 <svyash47@gmail.com>
2025-02-24 15:25:20 +0530

    Dayt3

--- p2.js
+++
@@ -1,3 +0,0 @@
-const fs = require("fs");
-const data = "I am new file";
-fs.writeFileSync("./data.txt", data);
--- p4.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-fs.readFile("./data.txt", "utf8", (err, data) => {
-  if (err) {
-    console.error("Error reading file:", err);
-    return;
-  }
-  console.log(data);
-});
